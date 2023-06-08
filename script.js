9575ffd30b44c9d1b1df3fe9be55530c43e2a1de
MarsWasNotAvailable <116720820+MarsWasNotAvailable@users.noreply.github.com>
2023-06-08 11:15:28 +0400

    Update form to script requirements
    new input field required in script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-
-
-
 function onSubmit()
 {
-    var nom = document.getElementById("nom").value;
-    var email = document.getElementById("email").value;
+    var nom = document.getElementById("NameFirst").value;
+    var email = document.getElementById("Mail").value;
 
 }
-  
